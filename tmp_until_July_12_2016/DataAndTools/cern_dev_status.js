ebf4a3d7ab8b256461741947d109c4c85cd249ec
Arturos Sanchez Pineda <arturo.rodolfo.sanchez.pineda@cern.ch>
2016-07-14 19:24:33 +0200

    Remove tmp directory

--- tmp_until_July_12_2016/DataAndTools/cern_dev_status.js
+++
@@ -1,8 +0,0 @@
-// $Id$
-(function ($) { 
-  $(document).ready(function()
-  {
-	  var contactMail = $("#cern_dev_status_contact_mail").text().replace("[at]", "@");
-  	$("#cern_dev_status_contact_mail").html("<a href=\"mailto:" + contactMail + "\">" + contactMail + "</a>");
-  });
-})(jQuery)
